b4159752a291bbc7d200239968062f227349a92b
Keshav-1803 <keshavagarwalfabs@gmail.com>
2024-11-06 11:07:12 +0530

    Models Added

---
+++ src/models/bookModel.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const bookModel = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  excerpt: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  ISBN: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  subcategory: {
+    type: [String],
+    required: true,
+  },
+  reviews: {
+    type: Number,
+    default: 0,
+    comment: "Holds number of reviews of this book",
+  },
+  deletedAt: {
+    type: Date,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+  releasedAt: {
+    type: Date,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Book", bookModel);
